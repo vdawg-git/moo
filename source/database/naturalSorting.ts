2118c9419a56d744ec2f772b701f7bbc2f22efbc
VDawg <vdawg@tuta.io>
2025-03-09 16:12:19 +0100

    improve natural sorting

--- source/database/naturalSorting.ts
+++ source/database/naturalSorting.ts
@@ -1,5 +1,47 @@
 import { orderBy } from "natural-orderby"
 import type { TrackData } from "./types"
+import { logg } from "#/logs"
+
+/** Prefixs to remove at the start of a title for sorting purposes */
+const prefixes = [
+	"the",
+	"a",
+	"an",
+	"der",
+	"der",
+	"die",
+	"das",
+	"ein",
+	"eine",
+	"einen",
+	"einer",
+	"le",
+	"la",
+	"les",
+	"un",
+	"une",
+	"des",
+	"Il",
+	"Lo",
+	"La",
+	"Gli",
+	"I",
+	"Un",
+	"Una",
+	"El",
+	"La",
+	"Los",
+	"Las",
+	"Un",
+	"Una",
+	"De",
+	"Het",
+	"Een",
+	"Ten",
+	"Ta",
+	"To"
+]
+const startRg = new RegExp(`^(?:${prefixes.join("|")})\\s+`, "i")
 
 /**
  * Sorts an array of tracks naturally
@@ -10,10 +52,10 @@ export function sortTracks<
 	return orderBy(
 		tracks,
 		[
-			(t) => t.titlesort ?? t.title ?? t.id,
-			(t) => t.artistsort ?? t.artist,
-			(t) => t.albumartistsort ?? t.albumsort,
-			(t) => t.album
+			(t) => t.titlesort ?? t.title?.replace(startRg, "") ?? t.id,
+			(t) => t.artistsort ?? t.artist?.replace(startRg, ""),
+			(t) => t.albumartistsort,
+			(t) => t.albumsort?.replace(startRg, "") ?? t.album?.replace(startRg, "")
 		],
 
 		["asc", "asc", "asc", "asc"]
