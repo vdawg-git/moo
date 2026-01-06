40a848a84182ef0720bb43e0bca4826437466839
VDawg <vdawg@tuta.io>
2026-01-06 17:25:51 +0100

    improve button

--- source/components/button.tsx
+++ source/components/button.tsx
@@ -1,13 +1,13 @@
 import { useColors } from "#/hooks/useColors"
 import type { TextProps } from "@opentui/react"
 
-type Props = TextProps & { children: string }
+type Props = TextProps & { children: string | readonly string[] }
 
 export function BracketButton({ children, ...rest }: Props) {
 	const colors = useColors()
 
 	return (
-		<text {...rest}>
+		<text fg={colors.blue} {...rest}>
 			<span fg={colors.brightBlack}>[</span> {children}{" "}
 			<span fg={colors.brightBlack}>] </span>
 		</text>
