6af6ebce7076b963235ed0d049c4646ba6f534cf
VDawg <vdawg@tuta.io>
2026-01-06 17:28:26 +0100

    error screen: log automatically and ensure string type

--- source/components/errorScreen.tsx
+++ source/components/errorScreen.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react"
+import { enumarateError, logg } from "#/logs"
 import { CowSay } from "./cowsay"
 
-export function ErrorScreen({ error }: { error: Error | string }) {
+export function ErrorScreen({ error }: { error: unknown }) {
+	useEffect(() => {
+		logg.error(
+			"An error occured in the app (error screen)",
+			enumarateError(error)
+		)
+	}, [error])
+
 	return (
 		<box flexDirection="column">
-			<CowSay d>{error instanceof Error ? error.message : error}</CowSay>
+			<CowSay d>
+				{error instanceof Error ? error.message : String(error)}
+			</CowSay>
 			{error instanceof Error && (
 				<text>
 					<span fg="red">Stack:</span>
