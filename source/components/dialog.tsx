8c5d1fbc9f9744a886b8994da9416cabdaadd66e
VDawg <vdawg@tuta.io>
2026-01-06 19:22:18 +0100

    add dialog component

---
+++ source/components/dialog.tsx
@@ -0,0 +1,46 @@
+import { type BoxProps, useKeyboard } from "@opentui/react"
+import { type ReactNode } from "react"
+import { useColors } from "#/hooks/useColors"
+
+export type DialogProps = { open: boolean; onClose?: () => void } & BoxProps
+
+/**
+ * Does not automatically position itself in the center.
+ * OpenTui does not support a `fixed` mode, so we resolve to using `absolute`
+ */
+export function Dialog({ open, onClose, ...boxProps }: DialogProps): ReactNode {
+	const colors = useColors()
+
+	useKeyboard((key) => {
+		if (key.name === "escape") {
+			onClose?.()
+			return
+		}
+	})
+
+	return (
+		open && (
+			<box
+				minHeight={"100%"}
+				minWidth={"100%"}
+				width={"100%"}
+				height={"100%"}
+				zIndex={200}
+				position="absolute"
+				justifyContent="center"
+				alignItems="center"
+			>
+				<box
+					position="absolute"
+					border
+					borderStyle="rounded"
+					borderColor={colors.fg}
+					backgroundColor={colors.bg}
+					minWidth={15}
+					minHeight={15}
+					{...boxProps}
+				/>
+			</box>
+		)
+	)
+}
