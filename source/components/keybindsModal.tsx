28ae13e02d52ccf98cf6dd0711cffbf84314a7f4
VDawg <vdawg@tuta.io>
2026-01-10 00:25:36 +0100

    linting

--- source/components/keybindsModal.tsx
+++ source/components/keybindsModal.tsx
@@ -5,34 +5,34 @@ import { displayKeybinding } from "#/lib/keybinds"
 import type { ReactNode } from "react"
 
 export function KeybindsModal(): ReactNode {
-	const toDisplay = useMemo(getKeybinds, [])
-	const colors = useColors()
+  const toDisplay = useMemo(getKeybinds, [])
+  const colors = useColors()
 
-	return (
-		<box flexDirection="column" minWidth={30} backgroundColor={colors.bg}>
-			{toDisplay.map(([id, { label, keybindings }]) => (
-				<box
-					key={id}
-					alignItems="flex-start"
-					justifyContent="space-between"
-					flexDirection="row"
-				>
-					<text fg={colors.brightCyan}>{label}</text>
+  return (
+    <box flexDirection="column" minWidth={30} backgroundColor={colors.bg}>
+      {toDisplay.map(([id, { label, keybindings }]) => (
+        <box
+          key={id}
+          alignItems="flex-start"
+          justifyContent="space-between"
+          flexDirection="row"
+        >
+          <text fg={colors.brightCyan}>{label}</text>
 
-					<box flexDirection="column">
-						{keybindings.map((binding) => (
-							<text fg={colors.fg} key={binding.join("")}>
-								{displayKeybinding(binding)}
-							</text>
-						))}
-					</box>
-				</box>
-			))}
-		</box>
-	)
+          <box flexDirection="column">
+            {keybindings.map((binding) => (
+              <text fg={colors.fg} key={JSON.stringify(binding)}>
+                {displayKeybinding(binding)}
+              </text>
+            ))}
+          </box>
+        </box>
+      ))}
+    </box>
+  )
 }
 
 /** A function as this is a cyclical import */
 function getKeybinds() {
-	return appConfig.keybindings.entries().toArray()
+  return appConfig.keybindings.entries().toArray()
 }
